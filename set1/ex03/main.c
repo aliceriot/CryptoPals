fd6e3bee6e4c744df117340839540b61729a1c1a
Alice <alice.writes.wrongs@gmail.com>
2015-06-07 13:16:05 -0400

    It works! I have decoded! woo!

--- set1/ex03/main.c
+++ set1/ex03/main.c
@@ -4,48 +4,29 @@
 
 void main()
 {
+    // input string (from Matasano)
     char teststring[] = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
-    //int testcounts[26];
-    //float testfreqs[26];
-    //int numchars, i;
-    
-    float keyGuess = 0.0;
-    int byteArray[MAXLINE]; // for testing keyGuess
-    int numBytes;
+
+    // byte array, number of bytes, iterator
+    int byteArray[MAXLINE]; int numBytes; int i;
     numBytes = hexArrayToByteArray(teststring, byteArray);
-
-    keyGuess = findKey(teststring);
-
-    char hopefulCleartext[MAXLINE];
-    xorByteArray(keyGuess, numBytes, byteArray, hopefulCleartext);
-
-    printf("key guess: %d\ncleartext: %s\n", keyGuess, hopefulCleartext);
-
-
-
-
-
-/*     memset(testcounts, 0, 26 * sizeof(int)); */
-
-/*     numchars = charCount(teststring, testcounts); */
-/*     countsToFrequency(numchars, testfreqs, testcounts); */
-
-/*     for (i = 0; i < 26; i++) */
-/*         printf("%d\n", testcounts[i]); */
-/*     for (i = 0; i < 26; i++) */
-/*         printf("%f\n", testfreqs[i]); */
- 
-/*     printf("\n"); */
-
-/*     int numBytes; */
-/*     int byteArray[MAXLINE]; */
-
-/*     numBytes = hexArrayToByteArray(teststring, byteArray); */
     
-/*     for (i = 0; i < numBytes; i++) */
-/*         printf("%d\t", byteArray[i]); */
+    // find good keys (score < 0.06)
+    int numKeys;
+    int possibleKeys[256];
+    numKeys = findKey(teststring, possibleKeys);
 
+    // loop through keys, print out poss. cleartext (along with score)
+    char testKeyArray[MAXLINE];;
+    float score;
 
-
-    //printf("\n\nFreq score: %f\n", frequencyScore(testfreqs));
+    for (i = 0; i < numKeys; i++) {
+        xorByteArray(possibleKeys[i], numBytes, byteArray, testKeyArray);
+        score = testKey(byteArray, possibleKeys[i], numBytes);
+        printf("Key: %d \t Score: %f \t Message: %s\n", possibleKeys[i], score, testKeyArray);
+    }
 }
+    
+    
+
+
