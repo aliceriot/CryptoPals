b2a683677e1c84afe2bb332331f2b246ccb012d2
Alice <alice.writes.wrongs@gmail.com>
2015-06-17 19:32:31 -0400

    It's a bit done!

--- set1/ex04/python/findString.py
+++ set1/ex04/python/findString.py
@@ -1,11 +1,12 @@
 import sys
+from hexstring import HexString
 
 filename = sys.argv[1]
 
 with open(filename, 'r') as myfile:
-    raw = myfile.readlines()
+    raw = [line.strip() for line in myfile]
 
 
-hexArray = [hexString(i) for i in raw]
+hexArray = [HexString(i) for i in raw]
 
 
