bdd57c3357aa8cc8e01eaa62301302fa6e7fc7cc
Alice Pote <alice.writes.wrongs@gmail.com>
2015-08-17 13:45:56 -0400

    Can get keys! One of them works!

--- set1/Python/ex04/XOR.py
+++ set1/Python/ex04/XOR.py
@@ -1,5 +1,29 @@
-
-
 class Singlebyte(object):
     def __init__(self, hexstring):
-        self.hex
+        self.hexstring = hexstring
+        self.bytestring = bytearray.fromhex(hexstring)
+        self.onlyascii = True
+
+    def ahelp(self, n):
+        "returns true if n is a valid ascii character"
+        if n > 31 and n < 127:
+            return True
+        return False
+
+    def otheridea(self):
+        "only those which never fail to produce ascii"
+        potk = list(range(256))
+        for b in self.bytestring:
+            temp = list(filter(lambda x: self.ahelp(x ^ b), potk))
+            potk = temp
+        return potk
+
+def utility(keys, hexstring):
+    for i in keys:
+        print(''.join(list(map(chr, map(lambda x: x ^ i, hexstring)))))
+
+
+
+
+
+
