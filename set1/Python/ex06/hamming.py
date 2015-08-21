31b42ab858261a56ca08551b0ac11456a8479ee7
Alice Pote <alice.writes.wrongs@gmail.com>
2015-08-21 11:41:14 -0400

    we have a hamming!

--- set1/Python/ex06/hamming.py
+++ set1/Python/ex06/hamming.py
@@ -1,24 +1,6 @@
 
-
-def hamminghelp(numtup):
-    numone = bin(numtup[0])[2:]
-    numtwo = bin(numtup[1])[2:]
-    result = 0
-    for pair in zip(numone, numtwo):
-        if pair[0] != pair[1]:
-            result +=1
-    return result
-
 def distance(strone, strtwo):
-    sumit = 0
-    for pair in zip(map(ord, strone), map(ord, strtwo)):
-        print("pair:", pair)
-        print("dist:", hamminghelp(pair))
-        print("sum:", sumit)
-        sumit += hamminghelp(pair)
+    s1 = bytearray(strone)
+    s2 = bytearray(strtwo)
 
-
-
-    return sum(map(hamminghelp, zip(map(ord, strone), map(ord, strtwo))))
-    
-
+    return sum(bin(x^y).count('1') for x,y in zip(s1, s2))
