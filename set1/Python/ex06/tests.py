0cd0a4ac69ba60c19034b463c97bb970f7591ab7
Alice Pote <alice.writes.wrongs@gmail.com>
2015-08-21 11:41:04 -0400

    it works!

--- set1/Python/ex06/tests.py
+++ set1/Python/ex06/tests.py
@@ -3,7 +3,7 @@ from hamming import distance
 
 class HammingTests(unittest.TestCase):
     def test_example(self):
-        self.assertEqual(37, distance('this is a test', 'wokka wokka!!!'))
+        self.assertEqual(37, distance(b'this is a test', b'wokka wokka!!!'))
 
 
 if __name__ == '__main__':
