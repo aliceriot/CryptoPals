f5db2ad0e9ee08c33df7f5b255b616c1cc8ab019
Alice Pote <alice.writes.wrongs@gmail.com>
2015-08-21 11:48:04 -0400

    Keysieve!

--- set1/Python/ex06/keysieve.py
+++ set1/Python/ex06/keysieve.py
@@ -1,3 +1,6 @@
 from hamming import distance
 
-
+class Keysieve(object):
+    def __init__(self, ciphertext):
+        self.keys = {(keysize, 0) for keysize in range(2,41)}
+        self.ciphertext = bytearray(ciphertext)
