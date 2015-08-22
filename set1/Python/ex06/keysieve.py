b1d2ed384c8a408f3d6e3f9b930d4013c0646f07
Alice Pote <alice.writes.wrongs@gmail.com>
2015-08-21 20:48:08 -0400

    working on keysieve

--- set1/Python/ex06/keysieve.py
+++ set1/Python/ex06/keysieve.py
@@ -1,6 +1,12 @@
 from hamming import distance
 
 class Keysieve(object):
-    def __init__(self, ciphertext):
-        self.keys = {(keysize, 0) for keysize in range(2,41)}
-        self.ciphertext = bytearray(ciphertext)
+    def __init__(self, ciphertext, minkey, maxkey):
+        self.sizes = {(keysize, 0) for keysize in range(minkey, maxkey+1)}
+        self.ctext = bytearray(ciphertext, 'ascii')
+        self.sieve()
+
+    def sieve(self):
+        for s in self.sizes.keys:
+            self.sizes[s] = distance((self.ctext[0:s], self.ctext[s, 2*s]))/s
+
