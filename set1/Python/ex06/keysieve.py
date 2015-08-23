80ce2b4308cc229d48fa1d8f28425fe35db5e7ad
Alice Pote <alice.writes.wrongs@gmail.com>
2015-08-23 12:23:51 -0400

    keysieve sorts list of tuples, makes more sense

--- set1/Python/ex06/keysieve.py
+++ set1/Python/ex06/keysieve.py
@@ -2,11 +2,13 @@ from hamming import distance
 
 class Keysieve(object):
     def __init__(self, ciphertext, minkey, maxkey):
-        self.sizes = {keysize: 0 for keysize in range(minkey, maxkey+1)}
+        self.sizes = []
+        self.keys = range(minkey, maxkey+1)
         self.ctext = bytearray(ciphertext, 'ascii')
         self.sieve()
 
     def sieve(self):
-        for s in self.sizes.keys():
-            self.sizes[s] = distance(self.ctext[0:s], self.ctext[s:2*s])/s
+        for s in self.keys:
+            self.sizes.append((s, distance(self.ctext[0:s], self.ctext[s:2*s])/s))
+        self.sizes.sort(key=lambda s: s[1])
 
