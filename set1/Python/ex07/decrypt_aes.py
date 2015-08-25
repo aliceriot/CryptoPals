e69f3f0f1f7902d9fb8f912843ea1d60143a45c1
Alice Pote <alice.writes.wrongs@gmail.com>
2015-08-25 15:11:39 -0400

    Exercise 7 done!

--- set1/Python/ex07/decrypt_aes.py
+++ set1/Python/ex07/decrypt_aes.py
@@ -1,8 +1,12 @@
-
+from Crypto.Cipher import AES
+from Crypto import Random
+from base64 import b64decode
 
 with open('./ex07.txt') as f:
-    ciphertext = f.read()
+    ciphertext = b64decode(''.join([x.strip() for x in f.readlines()]))
 
 key = b'YELLOW SUBMARINE'
 
+cipher = AES.new(key)
 
+print(''.join(map(chr, cipher.decrypt(ciphertext))))
