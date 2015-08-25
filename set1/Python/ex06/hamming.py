6922dd3a0e1b38c324f7995d108800431f6daf4c
Alice Pote <alice.writes.wrongs@gmail.com>
2015-08-25 10:37:28 -0400

    added docstring, checked the test

--- set1/Python/ex06/hamming.py
+++ set1/Python/ex06/hamming.py
@@ -1,3 +1,7 @@
+"""
+provides the 'distance' function, which returns
+the hamming distance between two bytearrays
+"""
 
 def distance(s1, s2):
     return sum(bin(x^y).count('1') for x,y in zip(s1, s2))
