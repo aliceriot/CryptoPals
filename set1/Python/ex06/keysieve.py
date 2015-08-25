7ebc45d70f0032f1aa5318985d61688627222293
Alice Pote <alice.writes.wrongs@gmail.com>
2015-08-25 12:43:27 -0400

    refactoring keysieve

--- set1/Python/ex06/keysieve.py
+++ set1/Python/ex06/keysieve.py
@@ -1,14 +1,26 @@
 from hamming import distance
+from numpy import average as avg
 
 class Keysieve(object):
     def __init__(self, ciphertext, minkey, maxkey):
-        self.sizes = []
+        self.scores = []
         self.keys = range(minkey, maxkey+1)
         self.ctext = ciphertext
         self.sieve()
 
     def sieve(self):
-        for s in self.keys:
-            self.sizes.append((s, distance(self.ctext[0:s], self.ctext[s:2*s])/s))
-        self.sizes.sort(key=lambda s: s[1])
+        for ksize in self.keys:
+            first = self.ctext[:ksize]
+            second = self.ctext[ksize:2*ksize]
+            self.scores.append((ksize, distance(first, second) / ksize))
 
+
+    def trialsieve(self):
+        for ksize in self.keys:
+            c = [self.ctext[:ksize]]
+            sizerange = [ksize+i for i in range(int(len(self.ctext) / ksize))]
+            c += [self.ctext[i:ksize+i] for i in sizerange]
+            even = [i[1] for i in enumerate(c) if i[0] % 2 == 0]
+            odd = [i[1] for i in enumerate(c) if i[0] % 2 != 0]
+            score = avg([distance(z[0], z[1])/ksize for z in zip(even, odd)])
+            self.scores.append((ksize, score))
--- set1/Python/ex06/solve.py
+++ set1/Python/ex06/solve.py
@@ -8,25 +8,11 @@ from decrypt import Decrypt
 
 """this file actually solves the problem"""
 
+# read file into bytearray
 with open("./ex06.txt") as f:
     ciphertext = b64decode(''.join([l.strip() for l in f.readlines()]))
 
+# keysieve finds probable keysizes (using hamming distance)
 keysieve = Keysieve(ciphertext, 2, 40)
 
-#create blocks for the 4 best scoring keysize (5, 3, 2, 13)
-blocks = Blocks(ciphertext, [x[0] for x in keysieve.sizes[:4]])
 
-# #do Singlebyte on the blocks
-potentials = []
-for k in blocks.keysizes:
-    key = bytearray()
-    for b in blocks.blocks[k]:
-        temp = Singlebyte(b)
-        print(temp.results)
-        key.append(temp.bestkey)
-    decrypt = Decrypt(ciphertext, key)
-    potentials.append(decrypt.plaintext)
-
-with open('test.txt', 'w') as f:
-    for pot in potentials:
-        f.write(pot)
