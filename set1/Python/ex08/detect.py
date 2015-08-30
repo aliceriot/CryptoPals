029b3fb3b939d9660cfc80b8a4096bcc39e80e9a
Alice Pote <alice.writes.wrongs@gmail.com>
2015-08-30 12:21:07 -0400

    set up script to run

--- set1/Python/ex08/detect.py
+++ set1/Python/ex08/detect.py
@@ -1,6 +1,11 @@
-
+from ECB import ECBDetector
 
 with open("./ex08.txt") as f:
     hexstrings = [x.strip() for x in f.readlines()]
 
+goodscores = []
+for hexstring in hexstrings:
+    temp = ECBDetector(hexstring)
+    if temp.score > 0:
+        goodscores.append((temp.score, hexstring))
 
