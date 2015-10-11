62669df4408c8f7cb31b80a3f7f91e4a9d2ba04d
Alice Pote <alice.writes.wrongs@gmail.com>
2015-10-10 23:24:07 -0400

    Trying to use OpenSSL aes_128_ecb

--- set2/ex10/matasano.h
+++ set2/ex10/matasano.h
@@ -2,9 +2,10 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
+#include <openssl/aes.h>
 
-// this will before CBC block encryption/decryption with repeating-key
-// XOR (Vigenere's cipher).
+// this will perform CBC block encryption/decryption with AES-128
+// We're using OpenSSL in EBC mode.
 //
 // Input to the `cbc_cipher` struct should already be decoded (e.g. from
 // base64, hex, etc).
@@ -17,28 +18,21 @@ typedef struct cbc_cipher {
     unsigned int input_length;
 } cbc_cipher;
 
-void cbc_encrypt(cbc_cipher *cbc)
-{ // takes a cbc_cipher struct, performs encryption
-
-    size_t key_length = strlen(cbc->key);
-    
-    for (size_t i=0; i < cbc->input_length; i++) {
-        if (i < key_length)
-            cbc->ciphertext[i] = cbc->plaintext[i] ^ cbc->iv[i] ^ cbc->key[i];
-        else
-            cbc->ciphertext[i] = cbc->plaintext[i] ^ cbc->ciphertext[i-16] ^ cbc->key[i%16];
-    }
-}
-
 void cbc_decrypt(cbc_cipher *cbc)
 { // takes a cbc_cipher struct, performs decryption
 
-    size_t key_length = strlen(cbc->key);
+    // we're going to do something with this!
+    EVP_CIPHER_CTX ctx;
 
-    for (size_t i=0; i < cbc->input_length; i++) {
-        if (i < key_length)
-            cbc->plaintext[i] = cbc->ciphertext[i] ^ cbc->iv[i] ^ cbc->key[i];
-        else
-            cbc->plaintext[i] = cbc->ciphertext[i] ^ cbc->ciphertext[i-16] ^ cbc->key[i%16];
-    }
+    EVP_CIPHER_CTX_init(&ctx);
+    EVP_DecryptInit_ex(&ctx, EVP_aes_128_ecb(), cbc->key, cbc->iv);
+
+    /* size_t key_length = strlen(cbc->key); */
+
+    /* for (size_t i=0; i < cbc->input_length; i++) { */
+    /*     if (i < key_length) */
+    /*         cbc->plaintext[i] = cbc->ciphertext[i] ^ cbc->iv[i] ^ cbc->key[i]; */
+    /*     else */
+    /*         cbc->plaintext[i] = cbc->ciphertext[i] ^ cbc->ciphertext[i-16] ^ cbc->key[i%16]; */
+    /* } */
 }
