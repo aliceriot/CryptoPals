4b3e251e23b5bac20bc7095fab7b9b0aa4ea3ea9
Alice Pote <alice.writes.wrongs@gmail.com>
2015-10-12 20:36:33 -0400

    getting started!

--- set2/ex11/matasano.c
+++ set2/ex11/matasano.c
@@ -3,7 +3,12 @@
 
 int main()
 {
-    unsigned char key[16];
-    random_aes_key(16, key);
+    unsigned char *plaintext = "just a test whoooasdfasdf";
+    unsigned char ciphertext[strlen(plaintext)];
+    
+    random_encryption(plaintext, ciphertext);
 
+    for (int i =0; i < strlen(ciphertext); i++) {
+        printf("%x\t%x\n", plaintext[i], ciphertext[i]);
+    }
 }
--- set2/ex11/oracle.h
+++ set2/ex11/oracle.h
@@ -2,17 +2,41 @@
 #include <math.h>
 #include <stdlib.h>
 #include <time.h>
+#include <openssl/ssl.h>
 
 // CBC/ECB encryption oracle for Matasano problem 11
 
-void random_aes_key(int numbytes, unsigned char *key)
+void random_bytes(int numbytes, unsigned char *output)
 {
     srand(time(NULL));
 
     for (size_t i=0; i < numbytes; i++) {
-        key[i] = rand() % 256;
+        output[i] = rand() % 256;
     }
 }
 
 
+void random_encryption(unsigned char *plaintext, unsigned char *ciphertext)
+{
+
+    EVP_CIPHER_CTX ctx;
+    EVP_CIPHER_CTX_init(&ctx);
+    int outlength;
+
+    srand(time(NULL));
+    int decrypt_switch = rand() % 2;
+    char key[16];
+    random_bytes(16,key);
+
+    if (decrypt_switch == 1) {
+        char iv[16];
+        random_bytes(16, iv);
+        EVP_EncryptInit_ex(&ctx, EVP_aes_128_cbc(), NULL, key, iv);
+        EVP_EncryptUpdate(&ctx, ciphertext, &outlength, plaintext, strlen(plaintext));
+    }
+    else {
+        EVP_EncryptInit_ex(&ctx, EVP_aes_128_ecb(), NULL, key, NULL);
+        EVP_EncryptUpdate(&ctx, ciphertext, &outlength, plaintext, strlen(plaintext));
+    }
+}
 
